f2e58e5e154a7a7552161e42ac23bae134cd9ee9
a289459798 <289459798@qq.com>
2022-03-25 11:57:26 +0800

    vivo

--- android/src/main/java/com/reactlibrary/receive/VivoPushMessageReceiverImpl.java
+++ android/src/main/java/com/reactlibrary/receive/VivoPushMessageReceiverImpl.java
@@ -10,18 +10,18 @@ import com.vivo.push.sdk.OpenClientPushMessageReceiver;
 public class VivoPushMessageReceiverImpl extends OpenClientPushMessageReceiver {
     @Override
     public void onReceiveRegId(Context context, String s) {
-        Log.d("jjpush_vivo", " onReceiveRegId= " + s);
+        Log.d("jjpush", " onReceiveRegId= " + s);
     }
 
     @Override
     public void onTransmissionMessage(Context context, UnvarnishedMessage unvarnishedMessage) {
         super.onTransmissionMessage(context, unvarnishedMessage);
-        Log.d("jjpush_vivo", "收到透传通知 onTransmissionMessage= " + unvarnishedMessage.getMessage());
+        Log.d("jjpush", "收到透传通知 onTransmissionMessage= " + unvarnishedMessage.getMessage());
     }
 
     @Override
     public void onNotificationMessageClicked(Context context, UPSNotificationMessage unvarnishedMessage) {
         super.onNotificationMessageClicked(context, unvarnishedMessage);
-        Log.d("jjpush_vivo", "收到通知点击回调 onTransmissionMessage= " + unvarnishedMessage.toString());
+        Log.d("jjpush", "收到通知点击回调 onTransmissionMessage= " + unvarnishedMessage.toString());
     }
 }
